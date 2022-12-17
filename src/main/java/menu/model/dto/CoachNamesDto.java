2a07899f6f7351cbf91a47713bd9ad27f3cd1623
endlessmomo <soohyuk96@naver.com>
2022-12-17 14:05:31 +0900

    feat(CoachNamesDto) : 코치 이름은 전달하는 클래스 추가

---
+++ src/main/java/menu/model/dto/CoachNamesDto.java
@@ -0,0 +1,21 @@
+package menu.model.dto;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+public class CoachNamesDto {
+    private static final String COACH_NAMES_DELIMITER = ",";
+
+    private final String coachNames;
+
+    public CoachNamesDto(String coachNames) {
+        this.coachNames = coachNames;
+    }
+
+    public List <String> getCoachNames(){
+        return Arrays.stream(coachNames.split(COACH_NAMES_DELIMITER))
+                .collect(toList());
+    }
+}
