df597c831104326ca09b38f8273c88ea14d1b832
endlessmomo <soohyuk96@naver.com>
2022-12-17 17:03:39 +0900

    refactoring(MenuController) : 메소드명 정정

---
+++ src/main/java/menu/controller/MenuController.java
@@ -0,0 +1,50 @@
+package menu.controller;
+
+import static menu.util.Retry.execute;
+import java.util.List;
+import menu.domain.Coach;
+import menu.dto.CoachNamesDto;
+import menu.dto.CoachNamesDto;
+import menu.dto.InvalidMenusDto;
+import menu.dto.MenusDto;
+import menu.service.CoachService;
+import menu.service.MenuService;
+import menu.view.InputView;
+import menu.view.OutputView;
+
+
+public class MenuController {
+
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+
+    private final MenuService menuService;
+    private final CoachService coachService;
+
+    public MenuController(MenuService menuService, CoachService coachService) {
+        this.menuService = menuService;
+        this.coachService = coachService;
+    }
+
+    public void run() {
+        outputView.printStart();
+        execute(this::saveCoach);
+        execute(this::recommendMenu);
+        outputView.printEnd();
+    }
+
+    private void saveCoach() {
+        CoachNamesDto coachNameDto = execute(inputView::readCoachNames);
+        coachService.saveCoach(coachNameDto);
+    }
+
+    private void recommendMenu() {
+        List <Coach> coaches = coachService.findAll();
+        for (Coach coach : coaches) {
+            InvalidMenusDto invalidMenusDto = execute(inputView::readMenuNames, coach.getName());
+            coach.addInvalidMenus(invalidMenusDto.getMenus());
+        }
+        MenusDto result = menuService.recommendMenus(coaches);
+        outputView.printMenuMessage(result);
+    }
+}
