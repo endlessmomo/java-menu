fca58a3317980641937df9af8626997bb966fdcc
endlessmomo <soohyuk96@naver.com>
2022-12-17 16:33:42 +0900

    fix(Menu) : 전체적인 클래스 수정

--- src/main/java/menu/domain/Menu.java
+++ src/main/java/menu/domain/Menu.java
@@ -1,32 +1,33 @@
 package menu.domain;
 
-import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Objects;
 
-import java.util.Arrays;
-import java.util.List;
+public class Menu {
 
-public enum Menu {
-    JAPANESE("일식", Arrays.asList("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼")),
-    KOREAN("한식", Arrays.asList("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음")),
-    CHINESE("중식", Arrays.asList("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채")),
-    ASIAN("아시안", Arrays.asList("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜")),
-    WESTERN("양식", Arrays.asList("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니"));
+    private final String name;
 
-    private final String menuStyle;
-    private final List<String> kinds;
-
-    Menu(String menuStyle, List <String> kinds) {
-        this.menuStyle = menuStyle;
-        this.kinds = kinds;
+    public Menu(String name) {
+        this.name = name;
     }
 
-    public String getMenu(String menuStyle){
-        List<String> menus = Arrays.stream(values())
-                .filter(menu -> menu.menuStyle.equals(menuStyle))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("원하시는 스타일이 존재하지 않습니다."))
-                .kinds;
+    public String getName() {
+        return name;
+    }
 
-        return Randoms.shuffle(menus).get(0);
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Menu menu = (Menu) o;
+        return Objects.equals(name, menu.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
